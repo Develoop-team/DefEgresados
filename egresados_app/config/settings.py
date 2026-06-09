a1189cb784e93da33aed000addc3542585d3edf1
Margarita-Bravo <embravo3329@gmail.com>
2026-06-09 12:43:48 -0300

    subir comprobante tutor

--- egresados_app/config/settings.py
+++ egresados_app/config/settings.py
@@ -28,6 +28,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 STATIC_URL = 'core/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Application definition
 
--- egresados_app/core/urls.py
+++ egresados_app/core/urls.py
@@ -7,4 +7,6 @@ from core import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('pagar/', views.comprobante_cuota, name='pagar'),
+    path('admin_panel/gestion_comprobantes/', views.gestion_comprobantes, name='gestion_comprobantes'),
 ]
