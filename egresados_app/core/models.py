df120e777938d14b232281da47ffd52b44b84e1c
Evana Sabatella <eva.sabatella@gmail.com>
2026-06-14 21:21:39 -0300

    estilos panel admin solicitudes

--- egresados_app/core/models.py
+++ egresados_app/core/models.py
@@ -28,7 +28,7 @@ class Presupuesto(models.Model):
         ('aprobado', 'Aprobado'),
         ('rechazado', 'Rechazado'),
     ]
-    #colegio = models.ForeignKey(Colegio, on_delete=models.SET_NULL, null=True, blank=True)
+    colegio_guardado = models.ForeignKey(Colegio, on_delete=models.SET_NULL, null=True, blank=True)
     colegio = models.TextField(blank=True, null=True)
     provincia = models.TextField(blank=True, null=True)
     localidad = models.TextField(blank=True, null=True)
